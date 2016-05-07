65c0dbdd94512c2512da5a903c279ff8c807babb
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-05-07 17:23:27 +0100

    test(scaffolding): setup test scaffolding. First part of #2

---
+++ karma.conf.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const WATCH = process.argv.indexOf('--watch') > -1;
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: './',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/angular2-bootstrap-confirm.spec.ts'
+    ],
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'test/angular2-bootstrap-confirm.spec.ts': ['webpack', 'sourcemap']
+    },
+
+    webpack: {
+      devtool: 'inline-source-map',
+      resolve: {
+        extensions: ['', '.ts', '.js']
+      },
+      module: {
+        preLoaders: [{
+          test: /\.ts$/, loader: 'tslint?emitErrors=false&failOnHint=false', exclude: /node_modules/
+        }],
+        loaders: [{
+          test: /\.ts$/, loader: 'ts', exclude: /node_modules/
+        }]
+      }
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: WATCH,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: !WATCH
+  });
+};
---
+++ test/angular2-bootstrap-confirm.spec.ts
@@ -0,0 +1,14 @@
+import 'es6-shim';
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+import 'zone.js/dist/long-stack-trace-zone';
+import 'rxjs';
+import {describe, it} from '@angular/core/testing';
+
+describe('dummy test', () => {
+
+  it('should be a dummy test', () => {
+    expect(1 + 1).toBe(2);
+  });
+
+});
