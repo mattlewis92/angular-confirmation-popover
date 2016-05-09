252e83ec29dfc187121de451fbe47536bf5e89c6
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-05-09 20:40:59 +0100

    fix: fix typescript error that prevented the module from being imported

--- angular2-bootstrap-confirm.ts
+++ angular2-bootstrap-confirm.ts
@@ -1,5 +1,3 @@
-/// <reference path="./typings/browser.d.ts" />
-
 export * from './src/confirm.directive';
 export {ConfirmOptions} from './src/confirmOptions.provider';
 export * from './src/position.provider';
