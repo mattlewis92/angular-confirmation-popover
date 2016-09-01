9e00d307b2865e7ad0ca7e3bf79827379ce3c291
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-09-01 12:58:54 +0100

    chore: use mocha reporter

--- karma.conf.js
+++ karma.conf.js
@@ -70,10 +70,14 @@ module.exports = function(config) {
       }
     },
 
+    mochaReporter: {
+      showDiff: true
+    },
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage', 'karma-remap-istanbul'],
+    reporters: ['mocha', 'coverage', 'karma-remap-istanbul'],
 
     // web server port
     port: 9876,
