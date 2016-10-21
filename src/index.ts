c543bf1f720455d261aa18944087aa3e0889fcac
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-10-21 09:51:28 +0100

    fix(aot): export confirm directive

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export * from './confirm.module';
 // Private components, don't import or use these within your app, they are only exported so that AOT can work
 export {ConfirmPopover as PrivateComponent1} from './confirmPopover.component';
 export {Focus as PrivateComponent2} from './focus.directive';
+export {Confirm as PrivateComponent3} from './confirm.directive';
