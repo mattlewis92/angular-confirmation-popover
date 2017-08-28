cf2d16082fe764fd4a6bae7c1e3cf39ee71771e9
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-08-28 16:51:15 +0100

    test: switch to mocha for tests

--- test/angular-confirmation-popover.spec.ts
+++ test/angular-confirmation-popover.spec.ts
@@ -5,7 +5,7 @@ import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
 import 'zone.js/dist/sync-test';
 import 'zone.js/dist/proxy';
-import 'zone.js/dist/jasmine-patch';
+import 'zone.js/dist/mocha-patch';
 import 'rxjs';
 import {
   Component,
